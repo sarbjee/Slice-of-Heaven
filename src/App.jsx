1dad1472f956fec9b878ac6d0c9f321dd3e89c35
sarbjeet brar <39264573+sarbjee@users.noreply.github.com>
2023-12-05 17:56:43 -0700

    Routers set up done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+
+import Cart from "./features/cart/Cart";
+
+
+
+
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home />,
+  },
+  {
+    path:'/menu',
+    element:<Menu/>,
+    
+  },
+
+  {
+    path:'/cart',element:<Cart/>,
+  },
+
+
+ 
+]);
+
+
 function App() {
-  return <div>hello</div>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
