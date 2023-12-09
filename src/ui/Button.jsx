6c32f68bfe433342f940ee89b92bcf08164affbb
sarbjeet brar <39264573+sarbjee@users.noreply.github.com>
2023-12-08 19:35:37 -0700

    design fixed

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type, onClick }) {
   const base =
-    'inline-block text-sm rounded-full font-semibold uppercase tracking-wide transition-colors duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed';
+    'inline-block text-sm rounded-full font-semibold uppercase tracking-wide transition-colors duration-300 focus:outline-none focus:ring focus:ring-offset-2 disabled:cursor-not-allowed text-white';
 
   const styles = {
     primary:
       base +
-      ' bg-yellow-400 text-stone-800 hover:bg-yellow-300 focus:bg-yellow-300 px-4 py-3 md:px-6 md:py-4',
+      ' bg-olive-600 hover:bg-olive-500 focus:bg-olive-500 px-4 py-3 md:px-6 md:py-4',
     small:
       base +
-      ' bg-yellow-400 text-stone-800 text-xs hover:bg-yellow-300 focus:bg-yellow-300 px-4 py-2 md:px-5 md:py-2.5',
+      ' bg-olive-600 text-xs hover:bg-olive-500 focus:bg-olive-500 px-4 py-2 md:px-5 md:py-2.5',
     round:
       base +
-      ' bg-yellow-400 text-stone-800 px-2.5 py-1 text-sm hover:bg-yellow-300 focus:bg-yellow-300 md:px-3.5 md:py-2',
+      ' bg-olive-600 px-2.5 py-1 text-sm hover:bg-olive-500 focus:bg-olive-500 md:px-3.5 md:py-2',
     secondary:
-      'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
+      'inline-block text-sm rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide transition-colors duration-300 hover:bg-olive-600 hover:text-white focus:bg-olive-600 focus:text-white focus:outline-none focus:ring focus:ring-olive-200 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5',
   };
 
   if (to)
@@ -56,4 +56,5 @@ Button.defaultProps = {
 };
 
 export default Button;
+
 
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,22 +4,19 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b border-stone-200 bg-olive-500 px-4 py-3 uppercase sm:px-6">
-      <Link to="/menu" className="tracking-widest text-yellow-400">
-     SLICE OF HEAVEN 
+    <header className="flex items-center justify-between border-b border-stone-200 bg-red-800 px-4 py-3 uppercase sm:px-6">
+      <Link to="/menu" className="tracking-widest text-white">
+        SLICE OF HEAVEN 
       </Link>
-       
-
-     <SearchOrder />
-     
+      
+      <SearchOrder />
 
       <Username />
-     
-
-      
     </header>
   );
 }
 
 export default Header;
+
+
 
